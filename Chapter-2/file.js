9a5dbef4bead36316fc1583a85be0148750a9c57
Lamiaa Selim <lamiaaselim1896@gmail.com>
2024-02-06 03:14:08 +0200

    add:Ch02

---
+++ Chapter-2/file.js
@@ -0,0 +1,57 @@
+// // Exercise 01 -- Looping a Triangle
+// let emptyString = "";
+// let n = 8;
+// for (let i = 0; i <= n; i++) {
+//   for (let j = 0; j < i; j++) {
+//     emptyString += "#";
+//   }
+
+//   emptyString += "\n";
+// }
+// console.log(emptyString);
+
+// // Exercise 02 -- CheesBoard
+// let emptyString = "";
+// let n = 8;
+// let hashtag = true;
+
+// for (let i = 0; i < n; i++) {
+//   if (hashtag) {
+//     emptyString += "########\n";
+//     hashtag = false;
+//   } else {
+//     emptyString += " ########\n";
+//     hashtag = true;
+//   }
+// }
+
+// console.log(emptyString);
+
+// // Exercise 03 -- FizzBuzz
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//       console.log("FizzBuzz");
+//       continue;
+//     }
+//     if (i % 3 === 0) {
+//       console.log("Fizz");
+//       continue;
+//     }
+//     if (i % 5 === 0) {
+//       console.log("Buzz");
+//       continue;
+//     }
+//     console.log(i);
+//   }
+
+// // Exercise 01 -- Looping a Triangle left
+// let emptyString = "";
+// let n = 8;
+// for (let i = 8; i > 0; i--) {
+//   for (let j = 0; j < i; j++) {
+//     emptyString += "#";
+//   }
+
+//   emptyString += "\n";
+// }
+// console.log(emptyString);
